1c9aa936fc6bd3fde1581b49762489501dd1d349
Rolando Fernandez <rolando@ebi.ac.uk>
2018-04-13 17:57:16 +0100

    made config a class

--- config/config.js
+++ config/config.js
@@ -4,21 +4,23 @@
 const IngestClient = require('../client');
 const UuidListener = require('../listeners');
 
-const config = {
-    // config vars
-    INGEST_HOST : process.env.INGEST_HOST || 'localhost',
-    INGEST_PORT : process.env.INGEST_PORT || '8080',
-    RABBIT_HOST :process.env.RABBIT_HOST || 'localhost',
-    RABBIT_PORT : process.env.RABBIT_PORT || '15672',
-    PREFETCH_COUNT : parseInt(process.env.PREFETCH_COUNT || '1'),
-    UUID_EXCHANGE : 'ingest.accession.exchange',
-    UUID_QUEUE : 'ingest.metadata.accession.queue',
+class Config {
+    constructor(){
+        // config vars
+        this.INGEST_HOST = process.env.INGEST_HOST || 'localhost';
+        this.INGEST_PORT = process.env.INGEST_PORT || '8080';
+        this.RABBIT_HOST = process.env.RABBIT_HOST || 'localhost';
+        this.RABBIT_PORT = process.env.RABBIT_PORT || '15672';
+        this.PREFETCH_COUNT = parseInt(process.env.PREFETCH_COUNT || '1');
+        this.UUID_EXCHANGE = 'ingest.accession.exchange';
+        this.UUID_QUEUE = 'ingest.metadata.accession.queue';
 
-    // singletons
-    services: {
-        ingestClient : new IngestClient('http://' + this.INGEST_HOST + ':' + this.INGEST_PORT),
-        uuidListener : new UuidListener('amqp://' + this.RABBIT_HOST + ':' + this.RABBIT_PORT, this.ingestClient)
+        this.services = {};
+        this.services.ingestClient = new IngestClient('http://' + this.INGEST_HOST + ':' + this.INGEST_PORT);
+        this.services.uuidListener =  new UuidListener('amqp://' + this.RABBIT_HOST + ':' + this.RABBIT_PORT, this.services.ingestClient);
     }
-};
+}
+
+const config = new Config();
 
 module.exports = config;
